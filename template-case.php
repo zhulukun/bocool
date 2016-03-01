f02c72f4eb1b41d71df8d20defa1696e991b9d7b
zlk <1770195221@qq.com>
2016-03-01 10:21:35 +0800

    update

---
+++ template-case.php
@@ -0,0 +1,12 @@
+<?php
+/**
+*  
+*Template Name:案例
+*/
+?>
+
+<?php get_header(); ?>
+<?php get_header('masthead'); ?>
+<?php if ( function_exists( 'meteor_slideshow' ) ) { meteor_slideshow(); } ?>
+<?php get_footer('colophon'); ?>
+<?php get_footer(); ?>
