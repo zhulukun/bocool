e6d8fe238b5ff8c0706905012fcded907fc76f5f
zlk <1770195221@qq.com>
2016-03-05 23:00:49 +0800

    update

--- index.php
+++ index.php
@@ -1,17 +1,14 @@
 <?php get_header(); ?>
     
 <?php get_header('masthead'); ?>
-  <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/normalize.css">
+<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/normalize.css">
 
 <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/style.css" media="screen" type="text/css" />
 <div class="scroll_page">
     <div class="page first">
       <div style="margin-top:350px;">
         <div class="content" style="margin-top:350px">
-
           <p style="font-size:42px;font-weight:800;">体验评价&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp数据至上</p>
-
-
         </div>
       </div>
         <div class="row news hidden-xs hidden-sm" style="display: block;">
@@ -51,31 +48,14 @@
         </div> -->
 
     </div>
-   
-    <div class="page second">
-       <div class="content">
-           
-          <div class="row" style="color:#000">
-            <?php query_posts('showposts=3&category_name=index-3'); ?>
-              <?php while (have_posts()) : the_post(); ?>
-                        <div class="col-md-4" style="padding:0 50px;"><p><?php the_post_thumbnail(array(200,200));?> </p><p class="second-title"><?php the_title(); ?></p><div class="second-content"><?php the_content(); ?></div></div>
-              <?php endwhile;?> <?php wp_reset_query(); ?>
+   <div class="page second">
+        <div class="content" style="margin-top:67px;height:100%;width:100%;background-image:url(<?php echo get_template_directory_uri(); ?>/images/bg.png);background-repeat:repeat-x">
 
-          </div>
-        
-
-        </div>
-       <!--  <div class="control">
-            <a href="#" class="next">Next</a>
-            <a href="#" class="prev">Prev</a>
-        </div> -->
-  
-</div>
-
-    <div class="page third">
-        <div class="content" style="margin-top:120px;width:100%">
-            
-           <p>敬请期待</p>
+           <div class="row">
+              <div class="col-md-4" style="font-weight:300;font-size:14px;text-align:center;margin-top:250px;padding:0px 0px 0px 150px">博酷科技致力于通过专业化的技术手段和客观数据，提升智能消费电子的用户体验。从为制造商提供产品研发生产过程中的测试验证，到为最终用户提供购买选型咨询服务，再到售后维修服务提供支持，服务整个智能消费电子的生命周期。
+              </div>
+              <div class="col-md-8" style="padding:0px 150px 0px 0px;text-align:left"><img src="<?php echo get_template_directory_uri(); ?>/images/yemian.png" width="860px" height="640px"></div>
+           </div>
          
         </div>
 
@@ -84,6 +64,23 @@
             <a href="#" class="prev">Prev</a>
         </div> -->
     </div>
+    <div class="page third">
+       <div class="content">
+           
+          <div class="row" style="color:#000">
+            <?php query_posts('showposts=3&category_name=index-3'); ?>
+              <?php while (have_posts()) : the_post(); ?>
+                        <div class="col-md-4" style="padding:0 50px;"><p><?php the_post_thumbnail(array(200,200));?> </p><p class="second-title"><?php the_title(); ?></p><div class="second-content"><?php the_content(); ?></div></div>
+              <?php endwhile;?> <?php wp_reset_query(); ?>
+
+          </div>        
+        </div>
+       <!--  <div class="control">
+            <a href="#" class="next">Next</a>
+            <a href="#" class="prev">Prev</a>
+        </div> -->
+</div>
+    
 <script src="<?php echo get_template_directory_uri(); ?>/js/jquery.js"></script>
     <script src="<?php echo get_template_directory_uri(); ?>/js/jquery.mousewheel.js"></script>
     <script src="<?php echo get_template_directory_uri(); ?>/js/jquery.easing.js"></script>
